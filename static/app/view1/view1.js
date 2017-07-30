a291a6ff5b0919845b6c2f313c79d5f3dd74d60b
VincSch <vs@stroodel.com>
2017-07-30 16:17:01 +0200

    added time format

--- static/app/view1/view1.js
+++ static/app/view1/view1.js
@@ -2,63 +2,70 @@
 
 angular.module('myApp.view1', ['ngRoute', 'chart.js'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
-
-.controller('View1Ctrl', function($scope, $http) {
-
-    $http({method: 'GET', url: 'http://localhost:12345/measurements'}).success(function(data) {
-        var measureTimes = new Array();
-        var temperature = new Array();
-        var ph = new Array();
-
-        angular.forEach(data, function(item) {
-            measureTimes.push(item.time);
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
         });
-        $scope.measuremnts = measureTimes
+    }])
 
-        angular.forEach(data, function(item) {
-            temperature.push(item.temperature.value);
-        });
-        $scope.temp = temperature
+    .controller('View1Ctrl', function ($scope, $http) {
 
-        angular.forEach(data, function(item) {
-            ph.push(item.ph.value);
-        });
-        $scope.ph = ph
+        $http({method: 'GET', url: 'http://localhost:12345/measurements'}).success(function (data) {
+            var measureTimes = new Array();
+            var temperature = new Array();
+            var ph = new Array();
 
-        $scope.labels = measureTimes
-        $scope.series = ['Temp', 'PH'];
-        $scope.data = [temperature, ph]
-        $scope.onClick = function (points, evt) {
-            console.log(points, evt);
-        };
-        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-        $scope.options = {
-            scales: {
-                yAxes: [
-                    {
-                        id: 'y-axis-1',
-                        type: 'linear',
-                        display: true,
-                        position: 'left'
-                    },
-                    {
-                        id: 'y-axis-2',
-                        type: 'linear',
-                        display: true,
-                        position: 'right'
-                    }
-                ]
+            function formatChart(date) {
+                var d = new Date(date);
+                var dateString = d.getDate() + "." + d.getMonth() + "." + d.getFullYear()
+                    + " @ " + d.getHours() + ":" + d.getMinutes() + "." + d.getSeconds()
+                return dateString
             }
-        };
 
+            angular.forEach(data, function (item) {
+                measureTimes.push(formatChart(item.time));
+            });
+            $scope.measuremnts = measureTimes
+
+            angular.forEach(data, function (item) {
+                temperature.push(item.temperature.value);
+            });
+            $scope.temp = temperature
+
+            angular.forEach(data, function (item) {
+                ph.push(item.ph.value);
+            });
+            $scope.ph = ph
+
+            $scope.labels = measureTimes
+            $scope.series = ['Temp', 'PH'];
+            $scope.data = [temperature, ph]
+            $scope.onClick = function (points, evt) {
+                console.log(points, evt);
+            };
+            $scope.datasetOverride = [{yAxisID: 'y-axis-1'}, {yAxisID: 'y-axis-2'}];
+            $scope.options = {
+                scales: {
+                    yAxes: [
+                        {
+                            id: 'y-axis-1',
+                            type: 'linear',
+                            display: true,
+                            position: 'left'
+                        },
+                        {
+                            id: 'y-axis-2',
+                            type: 'linear',
+                            display: true,
+                            position: 'right'
+                        }
+                    ]
+                }
+            };
+
+        });
     });
-});
 
 
 /*.controller("LineCtrl", function ($scope) {
